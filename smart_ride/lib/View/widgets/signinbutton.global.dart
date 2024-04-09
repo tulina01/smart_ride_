767191b47d9bec8d44137a12a81f342e5350dcf3
kavindajh <hasithkavinda5@gmail.com>
2024-04-10 01:28:46 +0530

    add singup button

---
+++ smart_ride/lib/View/widgets/signinbutton.global.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+import '../../utils/global.colors.dart';
+
+class ButtonGlobal extends StatelessWidget {
+  const ButtonGlobal({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        // ignore: avoid_print
+        print('Login');
+      },
+      child: Container(
+        alignment: Alignment.center,
+        height: 55,
+        decoration: BoxDecoration(
+          color: GlobalColor.mainColor,
+          borderRadius: BorderRadius.circular(6),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              blurRadius: 10,
+            ),
+          ],
+        ),
+        child: const Text(
+          'Sign In',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
+    );
+  }
+}
